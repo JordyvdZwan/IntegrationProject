4fa900e0c4eb5209112fed16f337efd5e4031158
Vincent <Vincent@Vincent-Laptop>
2016-04-05 11:49:15 +0200

    Worked on implementing the connection

---
+++ Integration-Project/src/network/Connection.java
@@ -0,0 +1,65 @@
+package network;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.MulticastSocket;
+import java.net.InetAddress;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Connection extends Thread {
+	
+	MulticastSocket socket;
+	InetAddress paddress;
+	List<DatagramPacket> queuedpackets = new ArrayList<DatagramPacket>();
+	
+	public Connection(int portnumber, String mcaddress) {
+		
+		try {
+			InetAddress address = InetAddress.getByName(mcaddress);
+			socket = new MulticastSocket(portnumber);
+			socket.joinGroup(address);
+			
+		} catch(IOException e) {
+			e.printStackTrace();
+		}
+		this.start();
+	}
+	
+	//Tries to receive a new datagram, if it gets one, put it in the queue.
+	public void run() {
+		
+		byte[] buffer = new byte[1000];
+		DatagramPacket recv = new DatagramPacket(buffer, buffer.length);
+		try {
+			socket.receive(recv);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		queuedpackets.add(recv);
+		recv.setLength(buffer.length);
+	}
+	
+	//Sends a datagrampacket coming from the client
+	public void send(DatagramPacket packet) {
+		
+		try {
+			
+			socket.send(packet);
+			
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public DatagramPacket getFirstInQueue() {
+		
+		DatagramPacket selectedpacket = queuedpackets.get(0);
+		queuedpackets.remove(0);
+		
+		return selectedpacket;
+	
+	}
+}
---
+++ IntegrationProject/src/Controller/Connection.java
@@ -0,0 +1,8 @@
+package Controller;
+
+import java.net.DatagramSocket;
+import java.net.DatagramPacket;
+
+public class Connection {
+	
+}
