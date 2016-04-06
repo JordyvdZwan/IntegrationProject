4ec5ca37869cef6a7930f1c57ed25d8b5e6c3b2a
reinier <reinierstribos@gmail.com>
2016-04-06 10:40:18 +0200

    fout gevonden :)

--- Integration-Project/src/network/JRTVtest.java
+++ Integration-Project/src/network/JRTVtest.java
@@ -1,13 +1,35 @@
 package network;
 
+import static org.junit.Assert.*;
+
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.Assert.*; 
 
 public class JRTVtest {
+	JRTVPacket packet = null;
 
 	@Before
 	public void setup() {
-		
+		byte[] bytes = {00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 
+				00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 00000000, 
+				00000000, 00000000, 00000000, 00000000, 00000000};
+		packet = new JRTVPacket(bytes);
+	}
+	
+	@Test
+	public void testSYN() {
+		packet.setSyn(true);
+		byte[] pakket = packet.toByteArray();
+		byte syn = (byte) 128;
+		assertEquals(pakket[19], syn);
+	}
+	
+	@Test
+	public void testACKSYN() {
+		packet.setAck(true);
+		packet.setSyn(true);
+		byte[] pakket = packet.toByteArray();
+		byte syn = (byte) 192;
+		assertEquals(pakket[19], syn);
 	}
 }
