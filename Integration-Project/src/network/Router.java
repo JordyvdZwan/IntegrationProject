97fbc06a8ef7f96d389b1aa6611d75f3e0573235
Jordy van der Zwan <jordyvdzwan@hotmail.com>
2016-04-06 10:07:51 +0200

    Worked on router and controller

--- Integration-Project/src/network/Router.java
+++ Integration-Project/src/network/Router.java
@@ -2,10 +2,20 @@ package network;
 
 import java.net.InetAddress;
 
+import application.Controller;
+
 public class Router {
 
+	private Controller controller;
 	
-	public InetAddress getIP() {
+	public Router(Controller controller) {
+		this.controller = controller;
+	}
+	
+	public InetAddress getIP(String client) {
+		if (client.equals("Anonymous")) {
+			return controller.getAddress();
+		}
 		return null;
 	}
 }
