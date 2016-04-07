733648a3665c498eb426a033a068f717a051150b
Thijs <Thijs@MagicMike>
2016-04-07 12:09:11 +0200

    Added shit

---
+++ Integration-Project/src/security/CreateEncryptedSessionPacket.java
@@ -0,0 +1,15 @@
+package security;
+
+public class CreateEncryptedSessionPacket {
+
+	/**
+	 * Krijgen tekst binnen
+	 * Creëer een key als deze er niet is
+	 * Encrypt message using key
+	 * hash en sign
+	 * concat
+	 * stuur terug
+	 * 
+	 */
+	
+}
