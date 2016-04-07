8f717dd6609929bf7785ae616b51c61009167820
Thijs <Thijs@MagicMike>
2016-04-07 18:36:10 +0200

    OFB statisch gemaakt

--- Integration-Project/src/security/OFB.java
+++ Integration-Project/src/security/OFB.java
@@ -2,23 +2,17 @@ package security;
 
 public class OFB {
 
-	//TODO Goede waardes instellen
-	private byte[] key = new byte[BLOCKSIZE];
 	private static final int BLOCKSIZE = 63;
 	private static final byte[] Oj =
 					{1,1,0,1,1,1,0,1,1,0,0,1,1,0,1,1,0,1,0,1,0,
 					 1,1,0,1,0,1,1,0,0,1,1,1,0,1,0,0,1,0,1,0,0,
 					 1,0,1,0,0,1,0,0,1,1,0,0,1,0,1,1,0,1,0,0,0}; 
+
 	
-//	public OFB() {
-//		this.key = new DiffieHellman(BLOCKSIZE).getKey().toByteArray();
-//	}
-	
-	public byte[] EnDecrypt(byte[] message) {
+	public static byte[] EnDecrypt(byte[] message, byte[] key) {
 		/**
 		 * OFB
 		 */
-				
 		int m = message.length; 				//Length of the message to encrypt.
 		byte[] result = new byte[m];			//Encrypted message in the form of a byte array
 		byte[][] blocks = new byte[(m/BLOCKSIZE) + 1][BLOCKSIZE];	//Array of the blocks used to encrypt.
@@ -34,20 +28,20 @@ public class OFB {
 			}
 		}
 		
-		/*
-		 * For loop om te printen van de de blocks created
-		 */
-		for (int i = 0; i < blocks.length; i++) {
-			System.out.print("{" );
-			for (int j = 0; j < BLOCKSIZE; j++) {
-				if (j != 0) {
-					System.out.print(", ");
-				}
-				System.out.print(blocks[i][j]);
-			}
-			System.out.print("}");
-		}
-		System.out.print("\n");
+//		/*
+//		 * For loop om te printen van de de blocks created
+//		 */
+//		for (int i = 0; i < blocks.length; i++) {
+//			System.out.print("{" );
+//			for (int j = 0; j < BLOCKSIZE; j++) {
+//				if (j != 0) {
+//					System.out.print(", ");
+//				}
+//				System.out.print(blocks[i][j]);
+//			}
+//			System.out.print("}");
+//		}
+//		System.out.print("\n");
 		
 		
 		for(int i = 0; i < m; i += BLOCKSIZE) {
@@ -91,56 +85,52 @@ public class OFB {
 	 * @param b
 	 * @return
 	 */
-	private byte[] xor(byte[] a, byte[] b) {
+	private static byte[] xor(byte[] a, byte[] b) {
 		byte[] result = new byte[a.length];
 		for (int i = 0; i < a.length; i++) {
 			result[i] = (byte)((int)a[i] ^ (int)b[i]);
 		}
 		return result;
 	}
-	
-	
-	public void setKey(byte[] key) {
-		this.key = key;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	/*
-	 * MAIN to test some stuff
-	 */
-	public static void main(String[] args) {
-		OFB hoi = new OFB();
-		hoi.setKey(new byte[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
-		
-		System.out.println("Encrypt: \n{1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1}");
-		byte[] result = hoi.EnDecrypt(new byte[]{1,1,1,0,1,1,1,0,0,0,0,0});
-		System.out.print("{" );
-		for (int i = 0; i < result.length; i++) {
-			if (i != 0) {
-				System.out.print(", ");
-			}
-		System.out.print(result[i]);
-		}
-		System.out.print("}\n");
-	
-		System.out.println("Decrypt:");
-		byte[] retur = hoi.EnDecrypt(result);
-		System.out.print("{" );
-		for (int i = 0; i < retur.length; i++) {
-			if (i != 0) {
-				System.out.print(", ");
-			}
-		System.out.print(retur[i]);
-		}
-		System.out.print("}");
-	}
 }
+	
+	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	/*
+//	 * MAIN to test some stuff
+//	 */
+//	public static void main(String[] args) {
+//		OFB hoi = new OFB();
+//		hoi.setKey(new byte[]{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0});
+//		
+//		System.out.println("Encrypt: \n{1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 1}");
+//		byte[] result = hoi.EnDecrypt(new byte[]{1,1,1,0,1,1,1,0,0,0,0,0});
+//		System.out.print("{" );
+//		for (int i = 0; i < result.length; i++) {
+//			if (i != 0) {
+//				System.out.print(", ");
+//			}
+//		System.out.print(result[i]);
+//		}
+//		System.out.print("}\n");
+//	
+//		System.out.println("Decrypt:");
+//		byte[] retur = hoi.EnDecrypt(result);
+//		System.out.print("{" );
+//		for (int i = 0; i < retur.length; i++) {
+//			if (i != 0) {
+//				System.out.print(", ");
+//			}
+//		System.out.print(retur[i]);
+//		}
+//		System.out.print("}");
+//	}
+//}
